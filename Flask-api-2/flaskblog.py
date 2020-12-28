3f7536695449a29b134d5d6a7fecbaa4df980450
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-28 21:45:33 +0530

    working with another flask web where 2 provided route for 2 html files and passed variable using jinja templating

---
+++ Flask-api-2/flaskblog.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Azhar',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'December 28,2020'
+    }
+    {
+        'author': 'Ajju',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 29,2020'
+    }
+]
+
+
+@app.route('/')
+def hello():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
