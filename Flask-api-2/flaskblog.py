6f3624dbc7473721e94ada3905711772b1adcf9e
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-29 11:45:32 +0530

    ignored one file , created form.py to handle our condition of input data we created 2 classes registration and login form where wtf form libraries used to proivide condition of our data its contain some special class like string filed validator etc , also created login and register html file, in our main code file we imported it also connected with our html files used flash messages also with some bootstrap design"

--- Flask-api-2/flaskblog.py
+++ Flask-api-2/flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '642c24502769cd43fb7953e17b1e173f'
 
 posts = [
     {
@@ -19,13 +22,36 @@ posts = [
 
 
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Page')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('/register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'aaa':
+            flash('You have been logged in ', 'success')
+            return redirect(url_for('home'))
+
+        else:
+            flash('Login Unsuccessful Please check Username and Password', 'danger')
+
+    return render_template('/login.html', title='Register', form=form)
 
 
 if __name__ == "__main__":
