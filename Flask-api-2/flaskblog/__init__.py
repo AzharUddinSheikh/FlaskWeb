85c595ae0fb005ca1ee6f8c4948212810ae27b0b
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-31 11:47:46 +0530

    our database have been connected to our htmml form requsest also our password has been genertaed hashed (bcrypt) with the help of module flask-bcypt

--- Flask-api-2/flaskblog/__init__.py
+++ Flask-api-2/flaskblog/__init__.py
@@ -1,12 +1,13 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '642c24502769cd43fb7953e17b1e173f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt =Bcrypt(app)
 
 from flaskblog import routes
