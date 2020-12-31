86bde086832ca078530106881478af20e9885670
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-31 12:58:19 +0530

    in this commit we have setup our login if user registered and login form request is in databse then we authenticate our user to home page also setup if user already logged in then we redirect register button and login button to home page

--- Flask-api-2/flaskblog/__init__.py
+++ Flask-api-2/flaskblog/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '642c24502769cd43fb7953e17b1e173f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt =Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
